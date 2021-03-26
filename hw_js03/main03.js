ab453c7a6142adfeb561acfcde7a73e325bdb843
SerhiiVakoliuk <s.vakoliuk@gmail.com>
2021-03-26 09:21:13 +0200

    Mar 26, 2021

---
+++ hw_js03/main03.js
@@ -0,0 +1,6 @@
+//- За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
+
+let divArray = [];
+for (let i = 0; i <=9; i = i + 1){
+   document.write('<div>Привіт, я програмую</div>')
+}
